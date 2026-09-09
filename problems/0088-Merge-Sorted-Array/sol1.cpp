9d6e3c21aee6c6913d2ed96fc369acc43f9df4c6
DevDevotee-DD <dakshdudeja17@gmail.com>
2026-09-09 18:15:18 +0530

    Add sol1: 88. Merge Sorted Array (C++)

---
+++ problems/0088-Merge-Sorted-Array/sol1.cpp
@@ -0,0 +1,26 @@
+// ==========================================================
+// 88. Merge Sorted Array
+// Difficulty : Easy
+// Language   : C++
+// Solution   : #1
+// Runtime    : 0 ms (Beats 100%)
+// Memory     : 12.3 MB (Beats 86%)
+// Link       : https://leetcode.com/problems/merge-sorted-array/
+// ==========================================================
+
+            }
+        }
+                j--;
+                nums1[k] = nums2[j];
+            else{
+                i--;
+            } 
+                nums1[k] = nums1[i];
+            if(nums1[i] >= nums2[j]){
+        if(i!=-1 && j!=-1){
+    for(int k = (m+n-1); k>=0; k--){
+    int j = n-1;
+    int i = m-1;
+   
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+public:
